23dff70ff03c0b143dfba2c47ab05cd48244ccb4
Oleg Shvedov <www.goo5@me.com>
2015-09-16 16:26:24 +0300

    simply initial project version

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  huffman
+//
+//  Created by root_2 on 16.09.15.
+//  Copyright (c) 2015 root_2. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
